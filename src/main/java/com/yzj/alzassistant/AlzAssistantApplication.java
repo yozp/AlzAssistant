1f5b8ba8f1c5bb42d07dc4892b219ed6c163e307
yozp <a1371361879@outlook.com>
2025-11-09 13:59:04 +0800

    后端初始化

---
+++ src/main/java/com/yzj/alzassistant/AlzAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.yzj.alzassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlzAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlzAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzj/alzassistant/AlzAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzj.alzassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlzAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
