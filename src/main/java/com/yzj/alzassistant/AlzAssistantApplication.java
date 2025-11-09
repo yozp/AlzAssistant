5d6fd92764226a3e97af01ce876ba9ab6c80c9d7
yozp <a1371361879@outlook.com>
2025-11-09 14:01:26 +0800

    后端初始化

--- src/main/java/com/yzj/alzassistant/AlzAssistantApplication.java
+++ src/main/java/com/yzj/alzassistant/AlzAssistantApplication.java
@@ -2,12 +2,14 @@ package com.yzj.alzassistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class AlzAssistantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlzAssistantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlzAssistantApplication.class, args);
+    }
 
 }
